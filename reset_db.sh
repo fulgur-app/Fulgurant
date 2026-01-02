ff2a296fa391eb72c9787424947f10799b04a307
PRRPCHT <pierrepic@gmail.com>
2026-01-02 07:27:31 +0100

    Script update

--- reset_db.sh
+++ reset_db.sh
@@ -4,4 +4,5 @@ rm -f data/fulgurant.db
 rm -f data/fulgurant.db-shm
 rm -f data/fulgurant.db-wal
 
-sqlx database create
+# Not needed anymore as we create the DB if needed at startup
+# sqlx database create
